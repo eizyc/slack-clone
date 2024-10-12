420d9bc154f53de57cfe8d252865ec2cfa18cb4b
eizyc <yizhichen98@gmail.com>
2024-10-12 01:27:07 -0500

    feat(workspace): add invite modal

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -12,6 +12,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { Doc } from "../../../../convex/_generated/dataModel";
+
+import { InviteModal } from "./invite-modal";
 import { PreferencesModal } from "./preferences-modal";
 
 interface WorkspaceHeaderProps {
@@ -20,10 +22,17 @@ interface WorkspaceHeaderProps {
 };
 
 export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
+  const [inviteOpen, setInviteOpen] = useState(false);
   const [preferencesOpen, setPreferencesOpen] = useState(false);
 
   return (
     <>
+      <InviteModal
+        open={inviteOpen}
+        setOpen={setInviteOpen}
+        name={workspace.name}
+        joinCode={workspace.joinCode}
+      />
       <PreferencesModal 
         open={preferencesOpen} 
         setOpen={setPreferencesOpen} 
@@ -58,6 +67,7 @@ export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) =>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   className="cursor-pointer py-2"
+                  onClick={() => setInviteOpen(true)}
                 >
                   Invite people to {workspace.name}
                 </DropdownMenuItem>
