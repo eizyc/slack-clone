f3e38be27dc0dc32e810d816a4e3377eef0a75ce
eizyc <yizhichen98@gmail.com>
2024-10-14 01:04:45 -0500

    feat(chat-input): add image upload functionality

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -4,11 +4,13 @@ import dynamic from "next/dynamic";
 import { useRef, useState } from "react";
 
 import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useGenerateUploadUrl } from "@/features/upload/api/use-generate-upload-url";
 
 import { useChannelId } from "@/hooks/use-channel-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 import { Id } from "../../../../../../convex/_generated/dataModel";
+
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -34,7 +36,7 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const workspaceId = useWorkspaceId();
 
   const { mutate: createMessage } = useCreateMessage();
-
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl();
 
   const handleSubmit = async ({
     body,
@@ -53,6 +55,28 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
         body,
         image: undefined,
       };
+
+      if (image) {
+        const url = await generateUploadUrl({}, { throwError: true });
+
+        if (!url) {
+          throw new Error("Url not found");
+        }
+
+        const result = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": image.type },
+          body: image,
+        });
+
+        if (!result.ok) {
+          throw new Error("Failed to upload image");
+        }
+
+        const { storageId } = await result.json();
+
+        values.image = storageId;
+      }
 
       await createMessage(values, { throwError: true });
 
